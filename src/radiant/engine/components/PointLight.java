bdecb4809fdc0514a111032bcbf887f7f47a3a69
Julian Thijssen <julian.thijssen@student.uva.nl>
2015-08-31 18:11:20 +0200

    Added a maximum shadow distance to PointLights

--- src/radiant/engine/components/PointLight.java
+++ src/radiant/engine/components/PointLight.java
@@ -7,6 +7,7 @@ import radiant.engine.core.math.Vector3f;
 public class PointLight extends Component {
 	public static final float DEFAULT_ENERGY = 1.0f;
 	public static final float DEFAULT_DISTANCE = 5.0f;
+	public static final float DEFAULT_SHADOW_DISTANCE = 20.0f;
 	public static final boolean DEFAULT_CAST_SHADOWS = false;
 
 	public CubeMap shadowMap = new CubeMap(MapType.SHADOW_MAP);
@@ -15,6 +16,7 @@ public class PointLight extends Component {
 	
 	public float energy = DEFAULT_ENERGY;
 	public float distance = DEFAULT_DISTANCE;
+	public float shadowDistance = DEFAULT_SHADOW_DISTANCE;
 	public boolean castShadows = DEFAULT_CAST_SHADOWS;
 	
 	public PointLight() {
